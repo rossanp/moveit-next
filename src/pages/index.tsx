a800347a444a56ce5b781e46f4a2f40a5ae0741f
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-03-02 21:44:09 -0300

    Criação countdown
    Estilização do componente countdown.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import styles from '../styles/pages/Home.module.css'
@@ -13,6 +14,7 @@ export default function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
